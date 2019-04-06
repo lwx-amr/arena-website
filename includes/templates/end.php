25c0a9fd47d8f40253a4c7345ae9a041ad5541ae
Gamal Khaled <Gamal.Khaled11@gmail.com>
2019-04-06 15:30:17 +0200

    Removing unnecessary files.

--- includes/templates/end.php
+++
@@ -1,8 +0,0 @@
-
-<!-- Start Footer -->
-<div class="footer main-back">
-  <div class="container text-center">
-      <h6>All CopyRights &copy; Are Reserved - Made With <i class="fas fa-heart second-color"></i> by <span class="logo">Arena</span> Team</h6>
-  </div>
-</div>
-<!-- End Footer -->
