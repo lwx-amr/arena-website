c9c6c24fb701fa9835935f5557d31c1e08dedd24
Gamal Khaled <Gamal.Khaled11@gmail.com>
2019-04-13 20:19:59 +0200

    fixing all but "register.html" responsiveness issues.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-$(function(){
+$(function () {
+    // Adjust Pannel design
+    $('.pannel i').on('click', function () {
+        $('.pannel').toggleClass('active');
+    })
 
-  // Adjust Pannel design
-  $('.pannel i').on('click',function(){
-      $('.pannel').toggleClass('active');
-  })
+    let navCollapsed = true;
+    $('.burgerIcon').on('click', function () {
+        $('.header').animate({
+            height: navCollapsed ? "250px" : "70px"
+        }, 250);
 
+        navCollapsed = !navCollapsed;
+    });
 })
